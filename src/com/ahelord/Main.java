d630357e6c90fb3a1b46cc647d5b85f4d491584f
Leonardo Rodriguez <leonardo@booklick.co>
2021-03-15 23:07:25 -0500

    Readme

--- src/com/ahelord/Main.java
+++ src/com/ahelord/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int num = 10;
+        int factor = 2;
+        int total = num*factor;
+        System.out.println(total);
     }
 }
