268f0b2887b3b0cb589e2acd974143aeb757b149
Leonardo Rodriguez <leonardo@booklick.co>
2021-03-24 00:06:49 -0500

    Tipos de datos'

---
+++ src/com/ahelord/tiposdedatos/Main.java
@@ -0,0 +1,28 @@
+package com.ahelord.tiposdedatos;
+
+public class Main {
+    public static void main(String[] args) {
+        //enteros
+        byte myByte = 127;
+        short myShort = 32767;
+        int myInt = 2147483647;
+        long myLong = 922337203685478099L;
+        // numero de punto flotante
+        float myFloat = 3.4028235E+38F;
+        double myDouble = 3.7976931348623157E+307D;
+        // Boolean
+        boolean myBoolean = false;
+        // Caracteres
+        char myChar = 'z';
+        System.out.println(myByte);
+        System.out.println(myShort);
+        System.out.println(myInt);
+        System.out.println(myLong);
+        System.out.println(myFloat);
+        System.out.println(myDouble);
+        System.out.println(myBoolean);
+        System.out.println(myChar);
+
+
+    }
+}
