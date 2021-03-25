d980b12c9de1bf8e8bab264e685e01ed07667718
Leonardo Rodriguez <leonardo@booklick.co>
2021-03-24 23:01:28 -0500

    Operados y Variables

--- src/com/ahelord/tiposdedatos/Main.java
+++ src/com/ahelord/tiposdedatos/Main.java
@@ -14,6 +14,15 @@ public class Main {
         boolean myBoolean = false;
         // Caracteres
         char myChar = 'z';
+        // Los valores numericos pueden ser representados como binarios, octetos, decimales y hexadecimales
+        int myBinary = 0b101010;
+        short myOctal = 052;
+        long myHex = 0x2A;
+        char myUnicode = '\u0041';
+        System.out.println(myHex);
+        System.out.println(myUnicode);
+        System.out.println(myOctal);
+        System.out.println(myBinary);
         System.out.println(myByte);
         System.out.println(myShort);
         System.out.println(myInt);
@@ -22,7 +31,47 @@ public class Main {
         System.out.println(myDouble);
         System.out.println(myBoolean);
         System.out.println(myChar);
+        // multiples variables de un mismo tipo puede ser inicializadas simultaneamente
+        int myBigNumOne = 1231231233, myBigNumTwo = 234232342;
+        // Se puede incializar sin darle valor
+        int myBigNumWithoutValue;
 
+        // float e =1.2 no se puede ya que se considera sin la F al final double
+        //los Char se declaran con comilla simple
+        // boolean h = "true"; no se puede
+        // boolean i = 'false'; no se puede
+        // boolean j = 0 no se puede;
+        // boolean k = False no se puede
+        int a = 10, b = 1000;
+        System.out.println(a%b);
+        System.out.println(a%=b);
+        System.out.println(a/=b);
+        System.out.println((a+b)*a);
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(a--);
+        System.out.println(--a);
+        int c;
+        // El operador de incremento de sufijo
+        // significa que la expresión se evalúa
+        // primero utilizando el valor original
+        // de la variable y luego la variable se incrementa
+        c=a++;
+        System.out.println(String.format("a: %s", a));
+        System.out.println(String.format("c: %s",c));
+        // El operador de incremento de prefijo significa
+        // que la variable se incrementa primero y luego
+        // la expresión se evalúa utilizando el
+        // nuevo valor de la variabl
+        c=++a;
+        System.out.println(String.format("a: %s", a));
+        System.out.println(String.format("c: %s",c));
+        c=a--;
+        System.out.println(String.format("a: %s", a));
+        System.out.println(String.format("c: %s",c));
+        c=--a;
+        System.out.println(String.format("a: %s", a));
+        System.out.println(String.format("c: %s",c));
 
     }
 }
